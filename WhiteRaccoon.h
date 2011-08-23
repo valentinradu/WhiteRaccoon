a218f3346bb7f04b89bc075065ce99045d5a7cda
Valentin Radu <radu.v.valentin@gmail.com>
2011-08-23 19:35:46 +0300

    Project started. Added initial files

---
+++ WhiteRaccoon.h
@@ -0,0 +1,16 @@
+//
+//  WhiteRaccoon.h
+//  WhiteRaccoon
+//
+//  Created by Valentin Radu on 8/23/11.
+//  Copyright 2011 Valentin Radu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface WhiteRaccoon : NSObject {
+    
+}
+
+@end
---
+++ WhiteRaccoon.m
@@ -0,0 +1,14 @@
+//
+//  WhiteRaccoon.m
+//  WhiteRaccoon
+//
+//  Created by Valentin Radu on 8/23/11.
+//  Copyright 2011 Valentin Radu. All rights reserved.
+//
+
+#import "WhiteRaccoon.h"
+
+
+@implementation WhiteRaccoon
+
+@end
